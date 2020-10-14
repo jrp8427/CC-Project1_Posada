3150e178bea18509002a7e45a12da6abbf242797
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2020-10-14 11:01:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 function setup() {
- createCanvas (900, 900);
+ createCanvas (600, 600);
+ background (0, 0, 0);
 }
 
 function draw() {
 	let x = random (width);
 	let y = 0;
 	let speed = 2;
-	ellipse (x, y, 5, 5);
+	noStroke();
+	fill (255, 255, 255);
+	ellipse (x, y, 5);
 	this.y = y + speed;
 }
