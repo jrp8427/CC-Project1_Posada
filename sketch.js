89f0c8e6875b84312251dfbccb8f748149b04f60
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2020-10-20 22:29:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,46 @@
 let drops = [];
 let stars = [];
 let change, p, starFill, xLoc, yLoc;
+let screen = 1;
+let r, g, b;
 
 function setup() {
   createCanvas(900, 900);
   for (let i=0; i<500; i++){
     stars[i] = new Star(random(width), random(0, 700));
   }
-  for (let i=0; i<175; i++){
+  for (let i=0; i<200; i++){
     drops[i] = new Rain();
   }
 }
 
 function draw() {
-  field ();
-  for (let i=0; i < 500; i++) {
-    stars[i].display();
-    stars[i].twinkle();
-  }
-  moon ();
-  for (let i=0; i<175; i++){ //150 raindrops produced at a time
-    drops[i].display(); 
-    drops[i].fall(); 
-    if (drops[i].y > height + 200){
-      drops[i] = new Rain(); //repeating drops produced
+  if (screen == 1) {
+    nightField ();
+    for (let i=0; i < 500; i++) {
+      stars[i].display();
+      stars[i].twinkle();
     }
+    moon ();
+    for (let i=0; i<200; i++){ //150 raindrops produced at a time
+      drops[i].display(); 
+      drops[i].fall(); 
+      if (drops[i].y > height + 200){
+        drops[i] = new Rain(); //repeating drops produced
+      }
+    }
+  } else if (screen == 2) {
+    dayField (); 
+    sun ();
   }
 }
 
+function mousePressed () {
+  screen ++;
+  if (screen > 2) {
+    screen = 1;
+  }
+}
 
 class Rain {
   constructor () {
@@ -61,7 +74,7 @@ class Star {
   constructor (x, y) {
     this.xLoc = x;
     this.yLoc = y;
-    this.change = random (1, 3);
+    this.change = random (1, 4);
     this.starFill = random (255);
   }
 
@@ -85,11 +98,24 @@ class Star {
   }
 }
 
-function field () {
+function nightField () {
   let opac = 120;
   let x = 0; 
   let y = height;
-  background (15);
+  background (15); //night sky
+  for (y = height; y >= 700; y -= 5) { //field display
+    fill (51, 81, 60, opac); //dark muted green
+    noStroke();
+    rect (x, y, width, 15);
+    opac -= 3; //fade
+  }
+}
+
+function dayField () {
+  let opac = 255;
+  let x = 0; 
+  let y = height;
+  background (25, 158, 218); //blue sky
   for (y = height; y >= 700; y -= 5) { //field display
     fill (51, 81, 60, opac); //dark muted green
     noStroke();
@@ -104,4 +130,21 @@ function moon () {
   ellipse (750, 150, 100);
   fill (15);
   ellipse (765, 140, 100);
+}
+
+function sun () {
+  noStroke();
+  fill (255, 240, 55);
+  beginShape ();
+  vertex (750, 75);
+  vertex (775, 125);
+  vertex (825, 150);
+  vertex (775, 175);
+  vertex (750, 225);
+  vertex (725, 175);
+  vertex (675, 150);
+  vertex (725, 125);
+  endShape (CLOSE);
+  fill (255, 253, 55); 
+  //ellipse (750, 150, 100);
 }
