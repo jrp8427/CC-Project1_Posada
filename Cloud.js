e37755db15e44ea98f54b2ffcf363a95789501b6
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2020-10-23 20:32:38 -0400

    Create Cloud.js

---
+++ Cloud.js
@@ -0,0 +1,35 @@
+class Cloud {
+	constructor () {
+		this.x = random (-300, 700);
+		this.y = random (-75, 350);
+		this.s = this.y * .25;
+		this.move = this.s * 0.02;
+		this.change = random (.1, .3);
+	}
+
+	display() {
+		noStroke ();
+    	fill (255);
+    	ellipse (this.x, this.y, this.s, this.s * 0.8);
+   		ellipse (this.x + (this.s * (2 * this.change)), this.y + (this.s * this.change), this.s, this.s * 0.8);
+    	ellipse (this.x - (this.s * this.change), this.y + (this.s * this.change), this.s, this.s * 0.8);
+    	ellipse (this.x + (this.s * (0.5 * this.change)), this.y - (this.s * (0.8 * this.change)), this.s, this.s * 0.8);
+    	ellipse (this.x - (this.s * this.change), this.y - (this.s * this.change), this.s, this.s * 0.8);
+    	ellipse (this.x + (this.s * this.change), this.y, this.s, this.s * 0.8);
+    	ellipse (this.x - (this.s * (2 * this.change)), this.y, this.s, this.s * 0.8);
+	}
+
+	float() {
+		this.x += this.move;
+	}
+
+	reset() {
+		if (this.x > width + 75) {
+			this.x = random (-75, 0);
+			this.y = random (-75, 700);
+			this.s = this.y * 0.25;
+			this.move = this.s * 0.02;
+			this.change = random (.1, .3);
+		}
+	}
+}
