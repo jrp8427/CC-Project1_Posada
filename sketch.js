18095ad1c39fde4c86240840146d1e9c55e099ac
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2020-10-24 21:33:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 let drops = []; 
-let stars = [];
+let stars = []; 
 let clouds = []; 
+let leaves = [];
 let change, p, starFill, xLoc, yLoc; //star variables
-let screen = 1;
+let screen = 1; 
 
 function setup() {
   createCanvas(900, 900);
@@ -12,36 +13,53 @@ function setup() {
   for (let i=0; i<250; i++){
     drops[i] = new Rain();
   }
-  for (let i = 0; i<20; i++){
+  for (let i = 0; i<24; i++){
     clouds[i] = new Cloud();
+  }
+  for (let i = 0; i<7; i++){
+    leaves[i] = new Leaf();
   }
 }
 
 function draw() {
-  if (screen == 1) {
-    nightField ();
-    for (let i=0; i < 500; i++) {
-      stars[i].display();
-      stars[i].twinkle();
-    }
-    moon ();
-    for (let i=0; i<250; i++){ //150 raindrops produced at a time
-      drops[i].display(); 
-      drops[i].fall(); 
-      if (drops[i].y > height + 200){
-        drops[i] = new Rain(); //repeating drops 
+	if (screen == 1) {
+		nightField ();
+		for (let i=0; i < 500; i++) {
+			stars[i].display();
+			stars[i].twinkle();
+		}
+		moon ();
+		for (let i=0; i<250; i++){ //150 raindrops produced at a time
+			drops[i].display(); 
+			drops[i].fall(); 
+			if (drops[i].y > height + 200){
+			drops[i] = new Rain(); //repeating drops 
+		}
+	}
+	} else if (screen == 2) {
+      sunset ();
+    	sun ();
+    	dayField (); 
+   		for (let i=0; i<24; i++){
+    		clouds[i].display();
+    		clouds[i].float();
+    		clouds[i].reset(); //repeating 
+    	}
+    	push();
+      translate (50, height + 150);
+    	tree (0);
+      pop();
+      push();
+      translate (-80, height + 120);
+      tree (0);
+    	pop();
+      for (let i=0; i<7; i++){
+        leaves[i].display();
+        leaves[i].fall();
+        leaves[i].reset();
       }
     }
-  } else if (screen == 2) {
-    dayField (); 
-    sun ();
-    for (let i=0; i<20; i++){
-      clouds[i].display();
-      clouds[i].float();
-      clouds[i].reset(); //repeating clouds
-    }
   }
-}
 
 function mousePressed () {
   screen ++;
