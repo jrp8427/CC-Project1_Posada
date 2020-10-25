15eb0a07e61e804dc0491dc1dcc520a0034f77b7
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2020-10-24 21:33:21 -0400

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ function nightField () {
   let y = height;
   background (15); //night sky
   for (y = height; y >= 700; y -= 5) { //field display
-    fill (51, 81, 60, opac); //dark muted green
+    fill (32, 90, 50, opac); //dark muted green
     noStroke();
     rect (x, y, width, 15);
     opac -= 3; //fade
@@ -12,28 +12,58 @@ function nightField () {
 }
 
 function dayField () {
-  let opac = 220;
-  let x = 0;
-  let y = height;
-  background (25, 158, 218); //blue sky
-  for (y = height; y >= 700; y -= 5) { //field display
-    fill (51, 81, 60, opac); //dark muted green
-    noStroke();
-    rect (x, y, width, 15);
-    opac -= 4;
-  }
+	let opac = 50; 
+  	noFill();
+  	for (let y = 710; y <= height; y += 1) { //field display
+  		fill (32, 90, 50, opac); //dark muted green
+    	noStroke();
+    	rect (0, y, width, 5);
+    	opac += 1;
+    }
 }
 
+function sunset () {
+	let x = 0;
+  	for (let i = 0; i <= height; i++) { //i = every x value
+  		let y = 0; 
+  		let c1 = color (249, 65, 68); //red
+  		let c2 = color (242, 187, 5); //yellow 
+  		var inter = map (i, y, y + height, 0, 1); //inter = # b/w 0 and 1 based on y value
+  		var c = lerpColor (c1, c2, inter); //make c a color gradient b/w c1 and c2
+  		stroke (c);
+  		line (x, i, x+width, i);
+  	}
+  }
+
 function moon () {
-  noStroke();
-  fill (255);
-  ellipse (750, 150, 100); //white circle
-  fill (15);
-  ellipse (765, 140, 100); //black circle
+	noStroke();
+	fill (255);
+	ellipse (750, 150, 150); //white circle
+	fill (15);
+	ellipse (765, 140, 150); //black circle
 }
 
 function sun () {
-  noStroke();
-  fill (255, 253, 55); 
-  ellipse (750, 150, 100);
+	noStroke();
+	fill (242, 187, 5); 
+	ellipse (width/2, 750, 325, 300);
+}
+
+function tree(depth){ 
+	strokeWeight (90);
+    stroke (0);
+    if (depth < 10) { 
+    	line(0,0,0,-300); // base line 
+    	translate(0,-300); // connect branches
+      	scale(0.8); //smaller 
+      	push();
+      	rotate(.4); // to the right
+        tree(depth + 1); // new branch
+        pop();
+
+        push();
+        rotate(-.5); // to the left
+        tree(depth + 1);   // second new branch 
+        pop();       
+    }
 }
